b0864d4af900c36bf2646ca2fffc399c4df2cec5
Dylan <dleard@uvic.ca>
2018-04-10 14:17:35 -0700

    first commit

---
+++ src/Dashboard.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import uuid from 'uuid'
+
+class Dashboard extends Component {
+  state = {}
+  
+  render() {
+    return (
+      <div className='ui five column centered grid'>
+        <div className='column'>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+export default Dashboard;
